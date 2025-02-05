928b982b01a228251c2627ef5078c37b0bc2ef55
Krish kumar <160249989+krishkumar2006@users.noreply.github.com>
2025-02-05 22:00:37 +0500

    Update page.tsx

--- Day6/projectF/src/app/payment-success/page.tsx
+++ Day6/projectF/src/app/payment-success/page.tsx
@@ -4,12 +4,13 @@ import { useSearchParams } from "next/navigation";
 
 export default function PaymentSuccess() {
     const searchParams = useSearchParams();
-    const amount = searchParams.get("amount") || "0"; // Fallback to "0" if null
+    const amount = searchParams?.get("amount") || "0"; // Fallback to "0" if null
 
     return (
         <div className="flex items-center justify-center min-h-screen bg-black text-white px-6">
             <h1 className="text-4xl md:text-6xl font-bold text-center">
-                Thank you for purchasing <span className="text-green-400">${amount}</span>!
+                Thank you for purchasing{" "}
+                <span className="text-green-400">${amount}</span>
             </h1>
         </div>
     );
