2f96143b4b9b5c140f189577ee2e8aaae41ab962
Krish kumar <160249989+krishkumar2006@users.noreply.github.com>
2025-02-07 00:26:31 +0500

    Update page.tsx

--- Day6/projectF/src/app/page.tsx
+++ Day6/projectF/src/app/page.tsx
@@ -1,7 +1,6 @@
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
-import StripePayment from "@/components/stripepayment";
-import CheckoutPage from "@/components/CheckoutPage";
+
 
 
 
@@ -14,7 +13,7 @@ export default function  Home() {
      <Header/>
     <Footer/> 
     
-<StripePayment></StripePayment>
+
     
     </div>
     
